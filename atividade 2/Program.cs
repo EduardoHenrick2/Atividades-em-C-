a6a0fa30e474e13721a47f335ae3a6fed5c36b94
Eduardo Lopes <eduardohenrick900@gmail.com>
2026-04-28 20:35:33 -0300

    Finalizando a atividade 2

--- atividade 2/Program.cs
+++ atividade 2/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Atividade_02 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite um numero:");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O fatorial de {n} é {fatorial(n)}.");
+        }
+        
+        static int fatorial(int n) {
+            if(n == 0) return 0;
+
+            else if (n == 1) return 1;
+
+            else return n * fatorial(n-1);            
+        }
+    }
+
+}
